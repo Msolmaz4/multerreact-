912832e3b15defe0ed2d84f96713981c032cd60a
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-11-23 22:58:36 +0100

    consol

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,18 @@ const multer =require('multer')
 const upload = multer({dest:'./uploads/'})
 
 app.post('/uploadFile',upload.single('avatar'),(req,res)=>{
+    //burada geleni gormek icin consol yaptik
+   // console.log('reg.file',req.file)
+   //sonra burada gelen dosyanin tipimni bulfuk
+   let fileType = req.file.mimetype.split('/')[1]
+   //geleni gorduk filetype [ 'image', 'jpeg' ]
+   //console.log('filetype',fileType)
+   //sonra yeniden comsolda bakmak icin boylecc jpeh yapariy
+   //console.log('filetype',fileType[1]) sonra split yanin abirinci terin almaki cin []ekleriz
+   //sonra filename bir duyenleme yapariy
+   let newFileName = req.file.filename+'.'+fileType
+   console.log('filename',newFileName)
+
     res.send('hallo world')
 })
 
