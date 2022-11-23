aecb090fae2608df00ba6650980197bb5b314a09
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-11-23 23:34:12 +0100

    fs

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
+//bunu multer fs renamr yaptik resim geldi
+const fs = require('fs')
 //dokumanttan yaparak yaptim
 //upload yaptiktan sonra uploadas dosyasi yaptim
 //ilk commit bak postman resim gonderildi
 //body form data name avatar file secildi ve resim yapildi donus alindi
 const multer =require('multer')
 const upload = multer({dest:'./uploads/'})
+//aciklamsui atta var
+app.use(express.static('./uploads'))
 
 app.post('/uploadFile',upload.single('avatar'),(req,res)=>{
     //burada geleni gormek icin consol yaptik
@@ -18,9 +22,16 @@ app.post('/uploadFile',upload.single('avatar'),(req,res)=>{
    //console.log('filetype',fileType[1]) sonra split yanin abirinci terin almaki cin []ekleriz
    //sonra filename bir duyenleme yapariy
    let newFileName = req.file.filename+'.'+fileType
-   console.log('filename',newFileName)
-
+   //console.log('filename',newFileName)
+   //dosyayi yenifen adlandirmak iicn fs.rename kullandim
+   //bu arada unutm fs import etmeyi
+   fs.rename(`./uploads/${req.file.filename}`,newFileName,function() {
+    console.log('callback')
     res.send('hallo world')
+   })
+ //bundan sonra on trafda goruntulemek icn statailk yapmamaiu gerekiyor
+ //yukarida app.use(express.sattaic yaptik)
+   
 })
 
 const Port = 4300
